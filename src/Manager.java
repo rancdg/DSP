bb67bc244179d2503c3e1e28c6570c971c556830
Ran Dach <rancdg@gmail.com>
2015-05-05 19:29:32 +0300

    manager init

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Manager {
+	public static void main(String[] args) throws FileNotFoundException,
+	IOException, InterruptedException{
+		
+		recieveMessage();
+	}
+
+	private static void recieveMessage() {
+		// Receive messages
+        System.out.println("Receiving messages from MyQueue.\n");
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(myQueueUrl);
+        List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+        for (Message message : messages) {
+            System.out.println("  Message");
+            System.out.println("    MessageId:     " + message.getMessageId());
+            System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
+            System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
+            System.out.println("    Body:          " + message.getBody());
+            for (Entry<String, String> entry : message.getAttributes().entrySet()) {
+                System.out.println("  Attribute");
+                System.out.println("    Name:  " + entry.getKey());
+                System.out.println("    Value: " + entry.getValue());
+            }
+        }
+        System.out.println();		
+	}
+	
 	
 	
 }
