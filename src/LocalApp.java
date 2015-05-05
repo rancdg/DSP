95206bade4b35cf21b60ca9c54629ac003c00a89
Ran Dach <rancdg@gmail.com>
2015-05-05 18:42:02 +0300

    code cleaning

--- src/LocalApp.java
+++ src/LocalApp.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
 
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.s3.AmazonS3;
@@ -61,11 +63,11 @@ public class LocalApp{
 			//TODO: check if manager exists
 			if (!managerExists){
 				//createManager();
-				//loadToSQS();
+				loadToSQS(key);
 				managerExists = true;
 			}
-			//else
-			//loadToSQS();
+			else
+			loadToSQS(key);
 
 		}
 
@@ -85,7 +87,7 @@ public class LocalApp{
 
 		S3 = new AmazonS3Client(Credentials);
 		System.out.println("AmazonS3Client created.");
-		// If the bucket doesnt exist - will create it.
+		// If the bucket doesn't exist - will create it.
 		// Notice - this will create it in the default region :Region.US_Standard
 		if (!S3.doesBucketExist(bucketName)) {
 			S3.createBucket(bucketName);
@@ -113,12 +115,19 @@ public class LocalApp{
 		try {
 			String managerQueue1Url;
 
-			if (managerExists) {
+			if (!managerExists) {
 				// Create a queue
 				System.out.println("Creating a new SQS queue called MyQueue.\n");
 				CreateQueueRequest createQueueRequest = new CreateQueueRequest(
 						"managerQueue1"); //maybe extrapolate
 				managerQueue1Url = sqs.createQueue(createQueueRequest).getQueueUrl();
+			}
+			else {
+				// Create a queue
+				System.out.println("Creating a new SQS queue called MyQueue.\n");
+				CreateQueueRequest createQueueRequest = new CreateQueueRequest(
+						"managerQueue1"); //maybe extrapolate
+				managerQueue1Url = sqs.createQueue(createQueueRequest).getQueueUrl(); //TODO: use existing queue instead of creating one
 			}
 
 			// List queues
@@ -132,42 +141,8 @@ public class LocalApp{
 			S3Object obj = S3.getObject(new GetObjectRequest(bucketName, key));
 			String URI = obj.getObjectContent().getHttpRequest().getURI().toString();
 			System.out.println("Sending a message to managerQueue1.\n");
-			sqs.sendMessage(new SendMessageRequest(managerQueue1Url, "This is my message text."));
+			sqs.sendMessage(new SendMessageRequest(managerQueue1Url, URI));
 
-			// Receive messages
-			System.out.println("Receiving messages from MyQueue.\n");
-			ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(managerQueue1Url);
-			List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
-			for (Message message : messages) {
-				System.out.println("  Message");
-				System.out.println("    MessageId:     " + message.getMessageId());
-				System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
-				System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
-				System.out.println("    Body:          " + message.getBody());
-				for (Entry<String, String> entry : message.getAttributes().entrySet()) {
-					System.out.println("  Attribute");
-					System.out.println("    Name:  " + entry.getKey());
-					System.out.println("    Value: " + entry.getValue());
-				}
-			}
-			System.out.println();
-
-			// Delete a message
-			System.out.println("Deleting a message.\n");
-			String messageRecieptHandle = messages.get(0).getReceiptHandle();
-			sqs.deleteMessage(new DeleteMessageRequest(managerQueue1Url, messageRecieptHandle));
-
-			// Delete a queue
-			System.out.println("Deleting the test queue.\n");
-			sqs.deleteQueue(new DeleteQueueRequest(managerQueue1Url));
-		} catch (AmazonServiceException ase) {
-			System.out.println("Caught an AmazonServiceException, which means your request made it " +
-					"to Amazon SQS, but was rejected with an error response for some reason.");
-			System.out.println("Error Message:    " + ase.getMessage());
-			System.out.println("HTTP Status Code: " + ase.getStatusCode());
-			System.out.println("AWS Error Code:   " + ase.getErrorCode());
-			System.out.println("Error Type:       " + ase.getErrorType());
-			System.out.println("Request ID:       " + ase.getRequestId());
 		} catch (AmazonClientException ace) {
 			System.out.println("Caught an AmazonClientException, which means the client encountered " +
 					"a serious internal problem while trying to communicate with SQS, such as not " +
