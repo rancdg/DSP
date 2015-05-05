3aac272fae8dedbc30e55b17da4be4b91a32b346
Ran Dach <rancdg@gmail.com>
2015-05-05 18:17:26 +0300

    Uri +send message to queue

--- src/LocalApp.java
+++ src/LocalApp.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
@@ -31,45 +33,56 @@ public class LocalApp{
 	public static String inputFile;
 	public static String outputFile;
 	public static int n;
+	public static boolean managerExists = false;
 
 	public static void main(String[] args) throws FileNotFoundException,
 	IOException, InterruptedException{
+
+		Credentials = new PropertiesCredentials(
+				new FileInputStream(propertiesFilePath));
+		System.out.println("Credentials created.");
 
 		if (args.length == 4 && args[4].equals("terminate")) {
 			//TODO: terminate();
 		}
 
 		else if (args.length == 3) {
-			
+
 			inputFile = args[0];
 			outputFile = args[1];
-			
+
 			try {
 				n = Integer.parseInt(args[2]);
 			} catch (NumberFormatException e) {
 				System.err.println("Argument" + args[2] + " must be an integer.");
 				System.exit(1);
 			}
-			uploadUrlsToS3();
+			String key = uploadUrlsToS3();
+			//TODO: check if manager exists
+			if (!managerExists){
+				//createManager();
+				//loadToSQS();
+				managerExists = true;
+			}
+			//else
+			//loadToSQS();
+
 		}
-		
+
 		else {		
 			System.err.println("Invalid arguments");
 			System.exit(1);
 		}
-		
-		
-		//loadSQS();
+
+
+
 		//requestManager();
 
 	}
 
-	private static void uploadUrlsToS3() throws FileNotFoundException,
+	private static String uploadUrlsToS3() throws FileNotFoundException,
 	IOException, InterruptedException{
 
-		Credentials = new PropertiesCredentials(
-				new FileInputStream(propertiesFilePath));
-		System.out.println("Credentials created.");
 		S3 = new AmazonS3Client(Credentials);
 		System.out.println("AmazonS3Client created.");
 		// If the bucket doesnt exist - will create it.
@@ -83,6 +96,83 @@ public class LocalApp{
 		// Upload the file
 		S3.putObject(por);
 		System.out.println("File uploaded.");
+		return f.getName();
 
 	}
+
+	private static void loadToSQS(String key) {
+
+		AmazonSQS sqs = new AmazonSQSClient(Credentials);
+		Region usEast1 = Region.getRegion(Regions.US_EAST_1);
+		sqs.setRegion(usEast1);
+
+		System.out.println("===========================================");
+		System.out.println("Getting Started with Amazon SQS");
+		System.out.println("===========================================\n");
+
+		try {
+			String managerQueue1Url;
+
+			if (managerExists) {
+				// Create a queue
+				System.out.println("Creating a new SQS queue called MyQueue.\n");
+				CreateQueueRequest createQueueRequest = new CreateQueueRequest(
+						"managerQueue1"); //maybe extrapolate
+				managerQueue1Url = sqs.createQueue(createQueueRequest).getQueueUrl();
+			}
+
+			// List queues
+			System.out.println("Listing all queues in your account.\n");
+			for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+				System.out.println("  QueueUrl: " + queueUrl);
+			}
+			System.out.println();
+
+			// Send a message
+			S3Object obj = S3.getObject(new GetObjectRequest(bucketName, key));
+			String URI = obj.getObjectContent().getHttpRequest().getURI().toString();
+			System.out.println("Sending a message to managerQueue1.\n");
+			sqs.sendMessage(new SendMessageRequest(managerQueue1Url, "This is my message text."));
+
+			// Receive messages
+			System.out.println("Receiving messages from MyQueue.\n");
+			ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(managerQueue1Url);
+			List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+			for (Message message : messages) {
+				System.out.println("  Message");
+				System.out.println("    MessageId:     " + message.getMessageId());
+				System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
+				System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
+				System.out.println("    Body:          " + message.getBody());
+				for (Entry<String, String> entry : message.getAttributes().entrySet()) {
+					System.out.println("  Attribute");
+					System.out.println("    Name:  " + entry.getKey());
+					System.out.println("    Value: " + entry.getValue());
+				}
+			}
+			System.out.println();
+
+			// Delete a message
+			System.out.println("Deleting a message.\n");
+			String messageRecieptHandle = messages.get(0).getReceiptHandle();
+			sqs.deleteMessage(new DeleteMessageRequest(managerQueue1Url, messageRecieptHandle));
+
+			// Delete a queue
+			System.out.println("Deleting the test queue.\n");
+			sqs.deleteQueue(new DeleteQueueRequest(managerQueue1Url));
+		} catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException, which means your request made it " +
+					"to Amazon SQS, but was rejected with an error response for some reason.");
+			System.out.println("Error Message:    " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code:   " + ase.getErrorCode());
+			System.out.println("Error Type:       " + ase.getErrorType());
+			System.out.println("Request ID:       " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, which means the client encountered " +
+					"a serious internal problem while trying to communicate with SQS, such as not " +
+					"being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}
+	}		
 }
